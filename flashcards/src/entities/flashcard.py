ad586598b308fb06fc077c15515e8e057ac424c3
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-11-25 21:37:44 +0200

    Modify class flashcard

--- flashcards/src/entities/flashcard.py
+++ flashcards/src/entities/flashcard.py
@@ -1,7 +1,6 @@
 class Flashcard:
-    def __init__(self, front, back, user, collection, id=None):
+    def __init__(self, front, back, collection_id, id=None):
         self.id = id
         self.front = front
         self.back = back
-        self.user = user
-        self.collection = collection
+        self.collection_id = collection_id

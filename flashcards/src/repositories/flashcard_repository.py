9e7f2a222b1f75e51ce75d9b6e2f49899931ea9b
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-11-25 21:38:38 +0200

    Add methods for getting and adding flashcards to db

--- flashcards/src/repositories/flashcard_repository.py
+++ flashcards/src/repositories/flashcard_repository.py
@@ -4,4 +4,40 @@ from database_connection import get_database_connection
 
 class FlashcardRepository:
     def __init__(self, connection):
-        pass
+        self._connection = connection
+
+
+    def find_all(self):
+        cur = self._connection.cursor()
+
+        cur.execute("SELECT * FROM flashcards;")
+        rows = cur.fetchall()
+
+        return [Flashcard(row["front"], row["back"], row["collection_id"], row["id"]) for row in rows]
+
+
+    def find_by_collection_id(self, collection_id):
+        cur = self._connection.cursor()
+
+        cur.execute("SELECT * FROM flashcards WHERE collection_id=:collection_id;",
+                    {"collection_id":collection_id})
+        rows = cur.fetchall()
+
+        return [Flashcard(row["front"], row["back"], row["collection_id"], row["id"]) for row in rows]
+
+
+    def create(self, flashcard):
+        cur = self._connection.cursor()
+
+        sql = ("INSERT INTO flashcards (front, back, collection_id) "
+               "VALUES (:front, :back, :collection_id)")
+        cur.execute(sql, {
+            "front":flashcard.front,
+            "back": flashcard.back,
+            "collection_id":flashcard.collection_id
+            })
+
+        self._connection.commit()
+
+
+flashcard_repository = FlashcardRepository(get_database_connection())

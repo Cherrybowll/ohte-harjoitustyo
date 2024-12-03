ac68542865787be9fe8a6a6647eb8e40bcde24eb
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-12-03 22:36:13 +0200

    Add first tests for flashcard service

---
+++ flashcards/src/tests/services/flashcard_service_test.py
@@ -0,0 +1,17 @@
+import unittest
+from services.flashcard_service import flashcard_service
+
+class TestFlashcardService(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_set_message_works(self):
+        self.assertIsNone(flashcard_service._message)
+        flashcard_service.set_message("message")
+        self.assertEqual(flashcard_service._message, "message")
+    
+    def test_get_message_returns_message_and_resets(self):
+        flashcard_service.set_message("message")
+        message = flashcard_service.get_message()
+        self.assertEqual(message, "message")
+        self.assertIsNone(flashcard_service._message)

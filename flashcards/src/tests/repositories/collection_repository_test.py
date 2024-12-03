7cf9f6a59d985751a44e1ff1b45aced3a4d76a3c
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-12-03 22:16:51 +0200

    Add tests for collection and flashcard repositories

---
+++ flashcards/src/tests/repositories/collection_repository_test.py
@@ -0,0 +1,51 @@
+import unittest
+from repositories.collection_repository import collection_repository
+from entities.collection import Collection
+
+class TestCollectionRepository(unittest.TestCase):
+    def setUp(self):
+        collection_repository.delete_all()
+        collection_repository.create(Collection("collection", 1))
+
+    def test_find_by_creator_id_returns_one_if_only(self):
+        collections = collection_repository.find_by_creator_id(1)
+        self.assertEqual(len(collections), 1)
+        self.assertEqual(collections[0].name, "collection")
+    
+    def test_find_by_creator_id_returns_all_if_many(self):
+        collection_repository.create(Collection("collection1", 1))
+        collections = collection_repository.find_by_creator_id(1)
+
+        self.assertEqual(len(collections), 2)
+        self.assertEqual(collections[0].name, "collection")
+        self.assertEqual(collections[1].name, "collection1")
+    
+    def test_find_by_creator_id_doesnt_return_from_wrong_user(self):
+        collection_repository.create(Collection("collection1", 2))
+        collections = collection_repository.find_by_creator_id(1)
+
+        self.assertEqual(len(collections), 1)
+        self.assertEqual(collections[0].name, "collection")
+    
+    def test_find_all_returns_all(self):
+        collection_repository.create(Collection("collection1", 1))
+        collection_repository.create(Collection("collection2", 2))
+        collections = collection_repository.find_all()
+
+        self.assertEqual(len(collections), 3)
+        self.assertEqual(collections[0].name, "collection")
+        self.assertEqual(collections[1].name, "collection1")
+        self.assertEqual(collections[2].name, "collection2")
+    
+    def test_find_by_creator_id_returns_empty_list_if_nonexistent(self):
+        collections = collection_repository.find_by_creator_id(2)
+        self.assertEqual(len(collections), 0)
+    
+    def test_find_all_returns_empty_list_if_nonexistent(self):
+        collection_repository.delete_all()
+        collections = collection_repository.find_all()
+        self.assertEqual(len(collections), 0)
+    
+    def test_string_representation(self):
+        collection = Collection("collection", 1, 1)
+        self.assertEqual(str(collection), "Collection: 1, collection, 1")
---
+++ flashcards/src/tests/repositories/flashcard_repository_test.py
@@ -0,0 +1,53 @@
+import unittest
+from repositories.flashcard_repository import flashcard_repository
+from entities.flashcard import Flashcard
+
+class TestFlashcardRepository(unittest.TestCase):
+    def setUp(self):
+        flashcard_repository.delete_all()
+        flashcard_repository.create(Flashcard("front", "back", 1))
+    
+    def test_find_by_collection_id_returns_one_if_only(self):
+        flashcards = flashcard_repository.find_by_collection_id(1)
+
+        self.assertEqual(len(flashcards), 1)
+        self.assertEqual(flashcards[0].front, "front")
+
+    def test_find_by_collection_id_returns_all_if_many(self):
+        flashcard_repository.create(Flashcard("front1", "back1", 1))
+        flashcards = flashcard_repository.find_by_collection_id(1)
+
+        self.assertEqual(len(flashcards), 2)
+        self.assertEqual(flashcards[0].front, "front")
+        self.assertEqual(flashcards[1].front, "front1")
+
+    def test_find_by_collection_id_doesnt_return_from_wrong_collection(self):
+        flashcard_repository.create(Flashcard("front1", "back1", 1))
+        flashcard_repository.create(Flashcard("front2", "back2", 2))
+        flashcards = flashcard_repository.find_by_collection_id(1)
+
+        self.assertEqual(len(flashcards), 2)
+        self.assertEqual(flashcards[0].front, "front")
+        self.assertEqual(flashcards[1].front, "front1")
+
+    def test_find_all_returns_all(self):
+        flashcard_repository.create(Flashcard("front1", "back1", 1))
+        flashcard_repository.create(Flashcard("front2", "back2", 2))
+        flashcards = flashcard_repository.find_all()
+
+        self.assertEqual(len(flashcards), 3)
+        self.assertEqual(flashcards[0].front, "front")
+        self.assertEqual(flashcards[1].front, "front1")
+        self.assertEqual(flashcards[2].front, "front2")
+
+    def test_find_by_collection_id_returns_empty_list_if_nonexistent(self):
+        flashcard_repository.delete_all()
+        flashcards = flashcard_repository.find_by_collection_id(1)
+
+        self.assertEqual(len(flashcards), 0)
+
+    def test_find_all_returns_empty_list_if_nonexistent(self):
+        flashcard_repository.delete_all()
+        flashcards = flashcard_repository.find_all()
+
+        self.assertEqual(len(flashcards), 0)
